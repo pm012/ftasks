295f1ab3bcbbc08f80b5952459d0d05e24274cb4
serg <serg@orion>
2017-06-15 16:52:41 +0300

    Task2 implementation

--- fTasks/src/lesson3/Task1.java
+++ fTasks/src/lesson3/Task1.java
@@ -3,14 +3,18 @@ package lesson3;
 public class Task1 {
 
 	public static void main(String[] args) {
-		System.out.println(Math.sin(rad2Grad(30)));
-		System.out.println(Math.cos(rad2Grad(30)));
+		try {
+			Double grads = Double.parseDouble(args[0]);
+			System.out.println(Math.sin(rad2Grad(grads)));
+			System.out.println(Math.cos(rad2Grad(grads)));
+		} catch (Exception e) {
+			System.out.println("Incorrect parameter");
+		}
 
 	}
-	
-	public static Double rad2Grad(float grad){
-		System.out.println(grad*Math.PI/180);
-		return grad*Math.PI/180;
+
+	public static Double rad2Grad(Double grad) {
+		return grad * Math.PI / 180;
 	}
 
 }
